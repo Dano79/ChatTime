56773156ed699ccfb8992555551bc3b068b97155
danielodorizzi <danielsreef@me.com>
2018-02-11 22:41:32 -0700

    Finished login

---
+++ ChatTime/app/src/main/java/drizzidevs/chattime/Controller/LoginActivity.kt
@@ -0,0 +1,84 @@
+package drizzidevs.chattime.Controller
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import drizzidevs.chattime.R
+import drizzidevs.chattime.Services.AuthService
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        loginSpinner.visibility = View.INVISIBLE
+    }
+
+    fun loginLoginBtnClicked(view: View) {
+        enableSpinner(true)
+        val email = loginEmailText.text.toString()
+        val password = loginPasswordText.text.toString()
+        hideKeyboard()
+        if (email.isNotEmpty() &&  password.isNotEmpty()) {
+            AuthService.loginUser(this, email, password) { loginSuccess ->
+                if (loginSuccess) {
+                    AuthService.findUserByEmail(this) { findSuccess ->
+                        if (findSuccess) {
+                            enableSpinner(false)
+                            finish()
+                        } else {
+                            errorToast()
+                        }
+
+                    }
+                } else {
+                    errorToast()
+                }
+
+            }
+        } else {
+            Toast.makeText(this, "Please fill in both email and password", Toast.LENGTH_LONG).show()
+        }
+
+
+    }
+
+    fun loginCreateUserBtnClicked (view: View) {
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
+        finish()
+
+    }
+
+    fun errorToast() {
+        Toast.makeText(this, "The rabbit done died.",
+                Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean) {
+        if (enable) {
+            loginSpinner.visibility = View.VISIBLE
+        } else {
+            loginSpinner.visibility = View.INVISIBLE
+        }
+        loginLoginBtn.isEnabled = !enable
+        loginCreateUserBtn.isEnabled = !enable
+
+    }
+
+    fun hideKeyboard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        if (inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
+    }
+
+
+}
